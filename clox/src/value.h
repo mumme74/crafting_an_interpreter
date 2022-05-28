a5cff90c37d6df7852c8338a90e68af6a8506850
Fredrik Johansson <fredrik@mumme.se>
2022-05-29 00:06:17 +0200

    Started clox, part 2 of the book

---
+++ clox/src/value.h
@@ -0,0 +1,20 @@
+#ifndef VALUE_H
+#define VALUE_H
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+  int count;
+  int capacity;
+  Value *values;
+} ValueArray;
+
+
+void initValueArray(ValueArray *array);
+void freeValueArray(ValueArray *array);
+void writeValueArray(ValueArray *array, Value);
+void printValue(Value value);
+
+#endif // VALUE_H
